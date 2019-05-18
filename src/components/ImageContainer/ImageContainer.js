14f12e2738eac3de14eff3794e9c3f6bbaee3060
Thomas Roselyn <tgroselyn@gmail.com>
2019-05-18 13:28:27 -0500

    clarifying comments in ImageContainer component

--- src/components/ImageContainer/ImageContainer.js
+++ src/components/ImageContainer/ImageContainer.js
@@ -5,7 +5,7 @@ class ImageContainer extends Component {
 
     componentDidMount = () => {
         this.props.dispatch({type: 'FETCH_IMAGES'});
-    }
+    } //end componentDidMount
 
     handleNext = () => {
         //declare next index value
@@ -16,18 +16,18 @@ class ImageContainer extends Component {
         }
         //dispatch CHANGE_POSITION with next index value
         this.props.dispatch({type: 'CHANGE_POSITION', payload: nextPosition});
-    }
+    } //end handleNext
 
     handlePrev = () => {
         //declare prev index value
         let prevPosition = this.props.index - 1;
-        //when reaching the end of the array, set next index to 0
+        //when value gets below 0, set prev index to last possible index value in array
         if (prevPosition < 0) {
             prevPosition = this.props.images.length - 1;
         }
-        //dispatch CHANGE_POSITION with next index value
+        //dispatch CHANGE_POSITION with prev index value
         this.props.dispatch({ type: 'CHANGE_POSITION', payload: prevPosition });
-    }
+    } //end handlePrev
 
     render() {
         return (
@@ -36,13 +36,13 @@ class ImageContainer extends Component {
                 <h3>{this.props.images[this.props.index].title}</h3>
                 {/* image */}
                 <img src={this.props.images[this.props.index].path} alt={this.props.images[this.props.index].title} />
-                {/* prev and next buttons */}
+                {/* prev and next buttons -- functions above */}
                 <button onClick={this.handlePrev}>PREV</button>
                 <button onClick={this.handleNext}>NEXT</button>
                 {/* tags component */}
             </div>
         )
-    }
+    } //end render
 }
 
 const mapRedux = (redux) => {
